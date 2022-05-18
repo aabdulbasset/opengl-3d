781de5aaa2eef243dd161f8b58868a4e80df259f
Ahmed <aabdulbasset5@gmail.com>
2022-05-18 11:49:32 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+
+
+#include <windows.h>
+#include <GL/glut.h>
+#include <iostream>
+using namespace std;
+
+void display(){
+   // Draw a Red 1x1 Square centered at origin
+   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(-0.5f, -0.5f);    // x, y
+      glVertex2f( 0.5f, -0.5f);
+      glVertex2f( 0.5f,  0.5f);
+      glVertex2f(-0.5f,  0.5f);
+   glEnd();
+
+   glFlush();
+}
+int main()
+{             // Initialize GLUT
+   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   glutInitWindowSize(320, 320);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutMainLoop();           // Enter the event-processing loop
+   return 0;
+}
