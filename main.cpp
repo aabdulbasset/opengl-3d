aefb470a3b2365453871bf928569ebd6a24baf7d
zico-son <77364329+zico-son@users.noreply.github.com>
2022-05-22 17:47:31 +0200

    Add files via upload
    OpenGL 3D Chair model

--- main.cpp
+++ main.cpp
@@ -1,122 +1,127 @@
-
-#include <windows.h>
+#include <GL/gl.h>
 #include <GL/glut.h>
-#include <cmath>
-#include <iostream>
-using namespace std;
+#include "vertices.h"
 
-void init(int argc, char** argv)
-{
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(500, 500);
-	glutCreateWindow("3D Screen");
-	glEnable(GL_DEPTH_TEST);
-}
-
-float width = 0.15;
-int angle = 0;
-int xAxis=0;int yAxis=0;
+int rotateX = -60, rotateY = 30, rotateZ = 0;  // rotation angle  
+float sclaeX = 0.25, sclaeY = 0.25 , sclaeZ=0.25; // Set up scale
+int tx = 0, ty = 0, tz = 0;  // Translate
 void display() {
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clears color buffer just in case
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glTranslatef(0.0,0.0,-5.0f);
-   glRotatef(angle,xAxis,yAxis,0);
-   glBegin(GL_QUADS);
-    //Top
-    glColor3f(0.1f, 0.1f, 0.1f);;
-    glVertex3f(-1.0,1.0,0.15);
-    glVertex3f(-1.0,1.0,-0.15);
-    glVertex3f(1.0,1.0,-0.15);
-    glVertex3f(1.0,1.0,0.15);
+       
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //cube
+  /*  glLoadIdentity();             // Set up modelview transform, first shape. 
+    glTranslatef(-2, 0, 0);      // Move cube to left half of window.
+    
+    glRotatef(rotateZ,0,0,1);     // Apply rotations.
+    glRotatef(rotateY,0,1,0);
+    glRotatef(rotateX,1,0,0);
+    
+    glVertexPointer( 3, GL_FLOAT, 0, cubeCoords );  // Set data type and location, first shape.
+    glColorPointer( 3, GL_FLOAT, 0, cubeFaceColors );
 
-    //back
-    glColor3f(0.1f, 0.1f, 0.1f);;
-    glVertex3f(-1.0,1.0,-0.15);
-    glVertex3f(-1.0,-1.0,-0.15);
-    glVertex3f(1.0,-1.0,-0.15);
-    glVertex3f(1.0,1.0,-0.15);
+    glEnableClientState( GL_VERTEX_ARRAY ); // permission for using data e.g. giving access
+					    // to set a connection like a clinet-server 
+					    // enable use of array
+    glEnableClientState( GL_COLOR_ARRAY );
 
-    //front
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0,1.0,0.15);
-    glVertex3f(-1.0,-1.0,0.15);
-    glVertex3f(1.0,-1.0,0.15);
-    glVertex3f(1.0,1.0,0.15);
+    glDrawArrays( GL_QUADS, 0, 24 ); // Draw the first shape!
+   */ 
 
-    //bottom
-    glColor3f(0.1f, 0.1f, 0.1f);;
-    glVertex3f(-1.0,-1.0,0.15);
-    glVertex3f(-1.0,-1.0,-0.15);
-    glVertex3f(1.0,-1.0,-0.15);
-    glVertex3f(1.0,-1.0,0.15);
+    // ----------------------------------------------------------------------------------------------
+    // Quote of the day a great man once said:
+    // "You don't know until you know" Zico.
+    //-----------------------------------------------------------------------------------------------
+    glLoadIdentity();             // Set up modelview transform, second shape.
 
-    //left
-    glColor3f(0.1f, 0.1f, 0.1f);;
-    glVertex3f(1.0,-1.0,0.15);
-    glVertex3f(1.0,-1.0,-0.15);
-    glVertex3f(1.0,1.0,-0.15);
-    glVertex3f(1.0,1.0,0.15);
+   glTranslatef(tx, ty,tz );        // Chair in the middle 
 
-    //right
-    glColor3f(0.1f, 0.1f, 0.1f);;
-    glVertex3f(-1.0,-1.0,0.15);
-    glVertex3f(-1.0,-1.0,-0.15);
-    glVertex3f(-1.0,1.0,-0.15);
-    glVertex3f(-1.0,1.0,0.15);
-   glEnd();
-   glutSwapBuffers(); // executes commands
+   //  rotations.
+	glRotatef(rotateX,1,0,0);
+	glRotatef(rotateY,0,1,0);
+	glRotatef(rotateZ,0,0,1); 
+  // Scale 
+     glScalef(sclaeX,sclaeX,sclaeX);
 
+    glVertexPointer( 3, GL_FLOAT, 0, vertexCoords );  // Set data type and location, second shape.
+    glColorPointer( 3, GL_FLOAT, 0, vertexColors );
+    glEnableClientState( GL_VERTEX_ARRAY ); // permission for using data e.g. giving access
+					    // to set a connection like a clinet-server 
+					    // enable use of array
+    glEnableClientState( GL_COLOR_ARRAY );
+    glPolygonOffset(1,1); // 1 for factor 1 for unit  
+    glEnable(GL_POLYGON_OFFSET_FILL); // change the deepth of all faces to get more sharp edges 
+    
+    
+    glDrawElements( GL_QUADS, 400, GL_UNSIGNED_INT, facesArray ); // Draw the chair 
+    glDisable(GL_POLYGON_OFFSET_FILL);
+    
+    glDisableClientState( GL_COLOR_ARRAY );  //No more color 
+    glColor3f(0.752, 0.443, 0.164); 	  // edges color 
+    glLineWidth(1);
+    
+    glDrawElements( GL_LINES, 392, GL_UNSIGNED_INT, edgeElementArray );  // Draw the edges
+    
+    glutSwapBuffers();
 }
 
-void reshape(int w,int h){
-    glViewport(0,0,w,h);
+//Keys XD:
+void specialKeyFunction(int key, int x, int y) {
+    if ( key == GLUT_KEY_LEFT )
+       rotateY -= 15;
+    if ( key == GLUT_KEY_RIGHT )
+       rotateY += 15;
+    if ( key == GLUT_KEY_DOWN)
+       rotateX += 15;
+    if ( key == GLUT_KEY_UP )
+       rotateX -= 15;
+    if ( key == GLUT_KEY_PAGE_UP )
+       rotateZ += 15;
+    if ( key == GLUT_KEY_PAGE_DOWN )
+       rotateZ -= 15;
+    if ( key == GLUT_KEY_HOME )
+       rotateX = rotateY = rotateZ = 0;
+    if ( key == GLUT_KEY_F1)
+       sclaeX += 0.01;
+    if ( key == GLUT_KEY_F2)      
+       sclaeX -= 0.01; 
+
+    if ( key == GLUT_KEY_F3)      
+       tx -= 1; 
+    if ( key == GLUT_KEY_F4)
+       tx += 1;
+    if ( key == GLUT_KEY_F5)      
+       ty -= 1; 
+    if ( key == GLUT_KEY_F6)
+       ty += 1;     
+    if ( key == GLUT_KEY_F7)      
+       tz-= 1; 
+    if ( key == GLUT_KEY_F8)
+       tz += 1;
+
+    glutPostRedisplay();
+}
+
+// Mouse -> // Abdelbasset Deal 
+
+// initialization  
+
+void initGL() {
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60,1,2,200);
+    glOrtho(-4, 4, -2, 2, -2, 2);  // simple orthographic projection
     glMatrixMode(GL_MODELVIEW);
-
-}
-
-void keyPress(unsigned char button,int x,int y){
-    switch(button){
-    case 'w':
-        angle -= 10;
-        xAxis = 1;
-        yAxis = 0;
-        glutPostRedisplay();
-        break;
-    case 's':
-        angle += 10;
-        xAxis = 1;
-        yAxis = 0;
-        glutPostRedisplay();
-        break;
-
-    case 'a':
-        angle -= 10;
-        xAxis = 0;
-        yAxis = 1;
-        glutPostRedisplay();
-        break;
-
-    case 'd':
-        angle += 10;
-        xAxis = 0;
-        yAxis = 1;
-        glutPostRedisplay();
-        break;
-    }
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.5, 0.5, 0.5, 1);    
 }
 
 int main(int argc, char** argv) {
-   init(argc, argv);
-   glutDisplayFunc(display);
-   glutReshapeFunc(reshape);
-   glutKeyboardFunc(keyPress);
-   glutMainLoop();
-
-   return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(20,20);
+    glutCreateWindow("USE ARROW KEYS TO ROTATE; HOME KEY RESETS");
+    initGL();
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKeyFunction);
+    glutMainLoop();
+    return 0;
 }
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ main.cpp vertices.cpp -lglut -lGL -o a && ./a && rm -rf a
+
