abf3b98a0362f30e7e2587e9a4f81b2454f38489
zico-son <77364329+zico-son@users.noreply.github.com>
2022-05-22 18:15:10 +0200

    OpenGL 3D Chair model

---
+++ vertices.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream> 
+
+extern float vertexCoords[288];
+
+extern float vertexColors[288];
+
+extern int facesArray[400];
+
+extern int edgeElementArray[392];
+
+extern float cubeCoords[72];
+
+extern float cubeFaceColors[72];
+
